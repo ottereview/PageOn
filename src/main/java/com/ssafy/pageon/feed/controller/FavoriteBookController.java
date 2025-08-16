b3017114ea057406b3b9506aa3697ff2e8069530
Kangboom <jkl6147@naver.com>
2025-08-16 23:32:11 +0900

    feat: 인생책 컨트롤러 구현

---
+++ src/main/java/com/ssafy/pageon/feed/controller/FavoriteBookController.java
@@ -0,0 +1,64 @@
+package kakao.rebit.feed.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import kakao.rebit.common.annotation.AllowAnonymous;
+import kakao.rebit.feed.dto.request.update.UpdateFavoriteBookRequest;
+import kakao.rebit.feed.dto.response.FavoriteBookResponse;
+import kakao.rebit.feed.service.FavoriteBookService;
+import kakao.rebit.member.annotation.MemberInfo;
+import kakao.rebit.member.annotation.MemberInfoIfPresent;
+import kakao.rebit.member.dto.MemberResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/feeds/favorite-books")
+@Tag(name = "인생책 피드 API", description = "인생책 피드 관련 API")
+public class FavoriteBookController {
+    
+    private final FavoriteBookService favoriteBookService;
+    
+    public FavoriteBookController(FavoriteBookService favoriteBookService) {
+        this.favoriteBookService = favoriteBookService;
+    }
+    
+    @Operation(summary = "인생책 목록 조회", description = "인생책 목록을 조회합니다.")
+    @AllowAnonymous
+    @GetMapping
+    public ResponseEntity<Page<FavoriteBookResponse>> getFavoriteBooks(
+            @Parameter(hidden = true) @MemberInfoIfPresent MemberResponse memberResponse,
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        return ResponseEntity.ok().body(favoriteBookService.getFavoriteBooks(memberResponse, pageable));
+    }
+    
+    @Operation(summary = "인생책 조회", description = "인생책을 조회합니다.")
+    @GetMapping("/{favorite-id}")
+    public ResponseEntity<FavoriteBookResponse> getFavoriteBook(
+            @Parameter(hidden = true) @MemberInfo MemberResponse memberResponse,
+            @PathVariable("favorite-id") Long favoriteId) {
+        return ResponseEntity.ok().body(favoriteBookService.getFavoriteBookById(memberResponse, favoriteId));
+    }
+    
+    @Operation(summary = "인생책 수정", description = "인생책을 수정합니다.")
+    @PutMapping("/{favorite-id}")
+    public ResponseEntity<Void> updateMagazine(
+            @Parameter(hidden = true) @MemberInfo MemberResponse memberResponse,
+            @PathVariable("favorite-id") Long favoriteBookId,
+            @Valid @RequestBody UpdateFavoriteBookRequest updateFavoriteBookRequest) {
+        favoriteBookService.updateFavoriteBook(memberResponse, favoriteBookId,
+                updateFavoriteBookRequest);
+        return ResponseEntity.noContent().build();
+    }
+}
