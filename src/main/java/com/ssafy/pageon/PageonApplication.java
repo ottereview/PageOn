2c0f069d8c42f77cfd592580f4946c144a081d15
윤경일 <kidreamx@gmail.com>
2025-08-16 21:49:03 +0900

    chore: initial project setup

---
+++ src/main/java/com/ssafy/pageon/PageonApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.pageon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/pageon/PageonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.pageon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
