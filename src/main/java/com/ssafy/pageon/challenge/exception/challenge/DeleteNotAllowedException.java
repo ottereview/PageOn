e04e832288fe261f0c3c4689f1ac162819263c88
윤경일 <kidreamx@gmail.com>
2025-08-19 17:26:53 +0900

    feat: challenge exception 추가

---
+++ src/main/java/com/ssafy/pageon/challenge/exception/challenge/DeleteNotAllowedException.java
@@ -0,0 +1,12 @@
+package com.ssafy.pageon.challenge.exception.challenge;
+
+import kakao.rebit.common.exception.BusinessException;
+
+public class DeleteNotAllowedException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new DeleteNotAllowedException();
+
+    private DeleteNotAllowedException() {
+        super(ChallengeErrorCode.DELETE_NOT_ALLOWED);
+    }
+}
---
+++ src/main/java/com/ssafy/pageon/challenge/exception/challenge/FullException.java
@@ -0,0 +1,12 @@
+package com.ssafy.pageon.challenge.exception.challenge;
+
+import kakao.rebit.common.exception.BusinessException;
+
+public class FullException extends BusinessException {
+
+    public static final BusinessException EXCEPTION = new FullException();
+
+    private FullException() {
+        super(ChallengeErrorCode.FULL);
+    }
+}
