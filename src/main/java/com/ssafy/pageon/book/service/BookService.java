850979e833f917a3d440f8d94f01c075e376e4a3
kangboom <jkl6147@naver.com>
2025-08-20 19:46:39 +0900

    feat: Book 서비스 구현

---
+++ src/main/java/com/ssafy/pageon/book/service/BookService.java
@@ -0,0 +1,111 @@
+package kakao.rebit.book.service;
+
+import java.util.List;
+import java.util.Optional;
+import kakao.rebit.book.dto.AladinApiResponseListResponse;
+import kakao.rebit.book.dto.AladinApiResponseResponse;
+import kakao.rebit.book.dto.BookDetailResponse;
+import kakao.rebit.book.dto.BookResponse;
+import kakao.rebit.book.dto.BriefReviewResponse;
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.book.exception.book.BookNotFoundException;
+import kakao.rebit.book.mapper.BookMapper;
+import kakao.rebit.book.repository.BookRepository;
+import kakao.rebit.feed.entity.FavoriteBook;
+import kakao.rebit.feed.repository.FavoriteBookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AladinApiService aladinApiService;
+    private final FavoriteBookRepository favoriteBookRepository;
+
+    public BookService(BookRepository bookRepository, AladinApiService aladinApiService,
+            FavoriteBookRepository favoriteBookRepository) {
+        this.bookRepository = bookRepository;
+        this.aladinApiService = aladinApiService;
+        this.favoriteBookRepository = favoriteBookRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<BookResponse> getAllBooks() {
+        return bookRepository.findAll().stream()
+                .map(BookMapper::toBookResponse)
+                .toList();
+    }
+
+    @Transactional
+    public Page<BookResponse> searchAndSaveBooksByTitle(String title, Pageable pageable) {
+        AladinApiResponseListResponse bookList = aladinApiService.searchBooksByTitle(title, pageable);
+
+        List<Book> foundBooks = bookList.item().stream()
+                .map(book -> bookRepository.findByIsbn(book.isbn())
+                        .orElseGet(() -> bookRepository.save(BookMapper.toBookEntity(book))))
+                .toList();
+
+        List<BookResponse> bookResponses = foundBooks.stream()
+                .map(BookMapper::toBookResponse)
+                .toList();
+
+        return new PageImpl<>(bookResponses, pageable, bookList.item().size());
+    }
+
+    @Transactional
+    public BookResponse getBookDetail(String isbn) {
+        Book book = searchAndSaveBookByIsbn(isbn);
+        return BookMapper.toBookResponse(book);
+    }
+
+    private Book searchAndSaveBookByIsbn(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseGet(() -> {
+                    AladinApiResponseResponse bookResponse = aladinApiService.searchBookByIsbn(isbn);
+                    return saveBook(bookResponse);
+                });
+    }
+
+    private Book saveBook(AladinApiResponseResponse bookResponse) {
+        return bookRepository.save(BookMapper.toBookEntity(bookResponse));
+    }
+
+    @Transactional(readOnly = true)
+    public Book findBookByIdOrThrow(Long bookId) {
+        return bookRepository.findById(bookId)
+                .orElseThrow(() -> BookNotFoundException.EXCEPTION);
+    }
+
+    // 새로운 한줄평과 서평을 가져오는 메서드
+    @Transactional
+    public BookDetailResponse getBookDetailReview(String isbn) {
+        Book book = searchAndSaveBookByIsbn(isbn);
+        Optional<FavoriteBook> topFavoriteBook = favoriteBookRepository.findTopByBookOrderByLikesDesc(book);
+        return BookMapper.toBookDetailResponse(book, topFavoriteBook.orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<BriefReviewResponse> getBriefReviewsByIsbn(String isbn, Pageable pageable) {
+        return favoriteBookRepository.findAllByBookIsbnOrderByLikesDesc(isbn, pageable)
+                .map(BookMapper::toBriefReviewResponse);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Book> findBookIfBookIdExist(Long bookId) {
+        if (bookId != null) {
+            Book book = findBookByIdOrThrow(bookId);
+            return Optional.of(book);
+        }
+        return Optional.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public Book findByIsbnOrThrow(String isbn) {
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> BookNotFoundException.EXCEPTION);
+    }
+}
